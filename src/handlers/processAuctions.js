1aa3e0c1ab37fdfad5b2d82a26fac2406af52b5f
Jimmy Tong <jimmytbc@gmail.com>
2020-08-25 23:51:11 +0800

    First commit

---
+++ src/handlers/processAuctions.js
@@ -0,0 +1,24 @@
+import createError from 'http-errors';
+import { getEndedAuctions } from '../lib/getEndedAuctions';
+import { closeAuction } from '../lib/closeAuction';
+
+async function processAuctions(event, context) {
+    try{
+        const auctionsToClose = await getEndedAuctions();
+        const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+        await Promise.all(closePromises);
+        return {
+            closed: closePromises.length
+        };
+    } catch(error){
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
+    
+}
+
+export const handler = processAuctions;
+
+// sls logs -f processAuctions -t
+// sls invoke -f processAuctions -l
+
